3d87c84f546a55482c846cc84b516f79f795aac1
andi0b <andix25@gmail.com>
2019-12-13 17:34:19 +0100

    Day 6

---
+++ AdventOfCode/Solutions/Day6.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Solutions.Intcode;
+
+namespace AdventOfCode.Solutions
+{
+    [Aoc(6)]
+    public class Day6 : IAocDay
+    {
+        private Dictionary<string, string> _outerToInnerMap;
+        private IEnumerable<string> _allObjects;
+
+        public Day6(ILoader loader) => LoadInput(loader.ReadAllLines("Input.txt"));
+
+        public void LoadInput(string[] input)
+        {
+            var pairs = (
+                from line in input
+                let splitted = line.Split(')')
+                select (inner: splitted[0], outer: splitted[1])
+            ).ToList();
+
+            _outerToInnerMap = pairs.ToDictionary(x => x.outer, x => x.inner);
+
+            _allObjects = from pair in pairs
+                          from obj in new[] {pair.inner, pair.outer}
+                          select obj;
+        }
+        
+        public int SolvePart1Internal()
+        {
+            var distinctObjects = _allObjects.Distinct().ToList();
+
+            return distinctObjects.Select(CalculateOuterCount).Sum();
+            
+            int CalculateOuterCount(string obj)
+            {
+                for (int i = 0;; i++)
+                {
+                    if (!_outerToInnerMap.TryGetValue(obj, out obj))
+                        return i;
+                }
+            }
+        }
+
+        public int SolvePart2Internal()
+        {
+            var pathFromSanta = PathToRoot("SAN").ToList();
+            var pathFromMe = PathToRoot("YOU").ToList();
+
+            IEnumerable<string> PathToRoot(string obj)
+            {
+                while (_outerToInnerMap.TryGetValue(obj, out obj))
+                    yield return obj;
+            }
+            
+            var intersectionPathLength = pathFromMe.Intersect(pathFromSanta).Count();
+
+            return pathFromMe.Count + pathFromSanta.Count - intersectionPathLength * 2;
+        }
+
+
+        public string SolvePart1() => $"total number of direct and indirect orbits  {SolvePart1Internal()}";
+
+        public string SolvePart2() => $"minimum number of orbital transfers {SolvePart2Internal()}";
+    }
+}
---
+++ Tests/Day6Test.cs
@@ -0,0 +1,61 @@
+﻿using AdventOfCode;
+using AdventOfCode.Solutions;
+using Autofac;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class Day6Test
+    {
+        private readonly Day6 _solution = Program.CreateContainer().Resolve<Day6>();
+
+        [Fact]
+        public void Part1Example()
+        {
+            var example = new[]
+            {
+                "COM)B",
+                "B)C",
+                "C)D",
+                "D)E",
+                "E)F",
+                "B)G",
+                "G)H",
+                "D)I",
+                "E)J",
+                "J)K",
+                "K)L",
+            };
+            
+            _solution.LoadInput(example);
+
+            _solution.SolvePart1Internal().Should().Be(42);
+        }
+
+        [Fact]
+        public void Part2Example()
+        {
+            var example = new[]
+            {
+                "COM)B",
+                "B)C",
+                "C)D",
+                "D)E",
+                "E)F",
+                "B)G",
+                "G)H",
+                "D)I",
+                "E)J",
+                "J)K",
+                "K)L",
+                "K)YOU",
+                "I)SAN",
+            };
+
+            _solution.LoadInput(example);
+            
+            _solution.SolvePart2Internal().Should().Be(4);
+        }
+    }
+}
