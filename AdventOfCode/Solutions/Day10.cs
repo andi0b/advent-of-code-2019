8235b71744b923e27fe5bd821d1aecccd7cab5c6
andi0b <andix25@gmail.com>
2019-12-20 16:35:29 +0100

    Day 10

--- AdventOfCode/Solutions/Day10.cs
+++ AdventOfCode/Solutions/Day10.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Text;
 using AdventOfCode.Solutions.Intcode;
@@ -9,29 +10,117 @@ namespace AdventOfCode.Solutions
     [Aoc(10)]
     public class Day10 : IAocDay
     {
-        public string Input { get; set; }
+        public List<(int x, int y)> Asteroids { get; private set; }
 
-        public Day10(ILoader loader) => Input = loader.ReadAllText("Input.txt");
+        public Day10(ILoader loader) => Load(loader.ReadAllLines("Input.txt"));
 
+        public void Load(string[] input)
+        {
+            Asteroids =
+                input.Select((line, lineNum) => line.ToCharArray()
+                                                    .Select((chr, chrNum) => (x: chrNum, y: lineNum, isAsterioid: chr == '#')))
+                     .SelectMany(x => x)
+                     .Where(x => x.isAsterioid)
+                     .Select(asteroid => (asteroid.x, asteroid.y))
+                     .ToList();
+        }
+
+        public int CalculateVisibleAsteroidCount((int x, int y) reference, IEnumerable<(int x, int y)> otherAsteroids)
+        {
+            var polarCoordinates =
+                from cartesian in otherAsteroids
+                let polar = CalculatePolar(cartesian, reference)
+                select (cartesian, polar);
+
+            var groupedByAngle = polarCoordinates.GroupBy(x => x.polar.ang);
+
+            return groupedByAngle.Count();
+        }
+        
+        public ((int x, int y) asteroid, int visibleOthers) SolvePart1Internal()
+        {
+            var visibleAstroids =
+                from asteroid in Asteroids
+                let otherAsteroids = Asteroids.Except(new[] {asteroid})
+                let visibleOthers = CalculateVisibleAsteroidCount(asteroid, otherAsteroids)
+                orderby visibleOthers descending 
+                select (asteroid, visibleOthers);
+
+            return visibleAstroids.First();
+        }
 
         public string SolvePart1()
         {
-            var program = new IntcodeProgram(Input);
-            var computer = new IntcodeComputer(program, 1);
-            
-            computer.Run();
+            var bestAsteroid = SolvePart1Internal();
+            return $"selected asteroid: {bestAsteroid.asteroid.x},{bestAsteroid.asteroid.y} visible count: {bestAsteroid.visibleOthers}";
+        }
 
-            return computer.Outputs.Single().ToString();
+        public IEnumerable<(int x, int y)> VaporizeAstroids((int x, int y) reference, List<(int x, int y)> asteroids)
+        {
+            var vaporizedAstroids = new List<(int x, int y)>();
+            var existingAsteroids = asteroids;
+            
+            do
+            {
+                existingAsteroids = asteroids.Except(vaporizedAstroids).ToList();
+
+                var nextAsteroids =
+                    from asteroid in existingAsteroids
+                    let polarCoordinates = CalculatePolar(asteroid, reference)
+                    let distance = polarCoordinates.rad
+                    let correctedAngle = TurnAngle(polarCoordinates.ang, 90)
+                    group (asteroid, distance) by correctedAngle
+                    into groupedByAngle
+                    orderby groupedByAngle.Key
+                    select (from x in groupedByAngle
+                            orderby x.distance
+                            select x).First();
+
+                vaporizedAstroids.AddRange(nextAsteroids.Select(x => x.asteroid));
+
+            } while (existingAsteroids.Any());
+
+            return vaporizedAstroids;
+        }
+
+        public double TurnAngle(double polarAngle, double degreeOffset)
+        {
+            var angleOffset = Math.PI / 180 * degreeOffset;
+            var turnedAngle = polarAngle + angleOffset;
+
+            if (turnedAngle < 0)
+                return Math.PI * 2 - turnedAngle;
+
+            if (turnedAngle >= Math.PI * 2)
+                return turnedAngle - Math.PI * 2;
+
+            return turnedAngle;
         }
 
         public string SolvePart2()
         {
-            var program = new IntcodeProgram(Input);
-            var computer = new IntcodeComputer(program, 2);
+            var reference = SolvePart1Internal().asteroid;
+            var others = Asteroids.Except(new[] {reference}).ToList();
+            var varporized = VaporizeAstroids(reference, others);
 
-            computer.Run();
+            var vaporized200 = varporized.Skip(199).First();
+            return
+                $"The 200th asteroid to be vaporized is at {vaporized200.x},{vaporized200.y} (solution is {vaporized200.x * 100 + vaporized200.y})";
+        }
 
-            return computer.Outputs.Single().ToString();
+        public (double rad, double ang) CalculatePolar((int x, int y) point, (int x, int y) referencePoint = default)
+        {
+            var x = point.x - referencePoint.x;
+            var y = point.y - referencePoint.y;
+
+            var ang = Math.Atan2(y, x);
+
+            ang = ang > 0 ? ang : Math.PI * 2 + ang;
+            
+            return (
+                Math.Sqrt(x * x + y * y),
+                ang
+            );
         }
     }
 }
---
+++ Tests/Day10Test.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode;
+using AdventOfCode.Solutions;
+using Autofac;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class Day10Test
+    {
+        private readonly Day10 _solution = Program.CreateContainer().Resolve<Day10>();
+
+        [Theory]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(1, 1, 1.4142, 0.25)]
+        [InlineData(0, 1, 1, 0.5)]
+        [InlineData(1, 0, 1, 0)]
+        [InlineData(-1, 0, 1, 1)]
+        [InlineData(0, -1, 0, 1.5)]
+        [InlineData(-1, -1, 1.4142, 1.25)]
+        public void Check(int x, int y, double rad, double ang)
+        {
+            var polar = _solution.CalculatePolar((x, y));
+
+            polar.rad.Should().BeApproximately(rad, 0.001);
+            polar.ang.Should().BeApproximately(ang * Math.PI, 0.001);
+        }
+
+        [Theory]
+        [InlineData(new[]
+        {
+            @".#..#",
+            ".....",
+            "#####",
+            "....#",
+            "...##"
+        }, "selected asteroid: 3,4 visible count: 8")]
+        public void Part1(string[] input, string result)
+        {
+            _solution.Load(input);
+            _solution.SolvePart1().Should().Be(result);
+        }
+
+        [Theory]
+        [InlineData(new[]
+        {
+            ".#....#####...#..",
+            "##...##.#####..##",
+            "##...#...#.#####.",
+            "..#.....X...###..",
+            "..#.#.....#....##"
+        },"")]
+        public void VaporizeInOrder(string[] input, string result)
+        {
+            _solution.Load(input);
+            var station = (8, 3);
+
+            var vaporizedInorder = _solution.VaporizeAstroids(station, _solution.Asteroids.Except(new[] {station}).ToList());
+
+            vaporizedInorder.Should().BeEquivalentTo(new[]
+            {
+                //   1,      2,      3,       4,      5,       6,      7,       8,        9,
+                (8, 1), (9, 0), (9, 1), (10, 0), (9, 2), (11, 1), (12, 1), (11, 2), (15, 1),
+
+                //    1,       2,       3,       4,       5,       6,       7,       8,      9,
+                (12, 2), (13, 2), (14, 2), (15, 2), (12, 3), (16, 4), (15, 4), (10, 4), (4, 4),
+
+                //    1,     2,       3,     4,      5,      6,      7,      8,     9,
+                (2, 4), (2, 3), (0, 2), (1, 2), (0, 1), (1, 1), (5, 2), (1, 0), (5, 1),
+
+                (6, 1), (6, 0), (7, 0), (8, 0), (10, 1), (14, 0), (16, 1), (13, 3), (14, 3),
+            });
+
+        }
+
+        [Fact]
+        public void TurnAngleNegative()
+            => _solution.TurnAngle(Math.PI / 4, -90).Should().Be(Math.PI * 2 - Math.PI / 4);
+
+        
+        [Theory]
+        [InlineData(new[]
+        {
+            ".#..##.###...#######",
+            "##.############..##.",
+            ".#.######.########.#",
+            ".###.#######.####.#.",
+            "#####.##.#.##.###.##",
+            "..#####..#.#########",
+            "####################",
+            "#.####....###.#.#.##",
+            "##.#################",
+            "#####.##.###..####..",
+            "..######..##.#######",
+            "####.##.####...##..#",
+            ".#####..#.######.###",
+            "##...#.##########...",
+            "#.##########.#######",
+            ".####.#.###.###.#.##",
+            "....##.##.###..#####",
+            ".#.#.###########.###",
+            "#.#.#.#####.####.###",
+            "###.##.####.##.#..##"
+        }, "The 200th asteroid to be vaporized is at 8,2 (solution is 802)")]
+        public void Part2(string[] input, string result)
+        {
+            _solution.Load(input);
+            _solution.SolvePart2().Should().Be(result);
+        }
+
+
+    }
+}
